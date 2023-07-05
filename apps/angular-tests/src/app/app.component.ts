b9c1be65a0c459c1018f2c8dd866cdcff7200528
Saleh Loke <salehloke@Salehs-MacBook-Air.local>
2023-07-06 07:02:33 +0800

    init cypress component testing

---
+++ apps/angular-tests/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ionic-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-tests';
+}
